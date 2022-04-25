f6066e52210638b685d4b882fcac8f0d92f2e7d1
João Pedro Vital <joaopedrovitaldoctum@gmail.com>
2022-04-24 22:30:45 -0300

    feat: :art: renderizacao condicional na navbar se usuario logado ou nao

--- doit-front/src/components/NavBar.js
+++ doit-front/src/components/NavBar.js
@@ -5,33 +5,39 @@ import { Link as RouterLink } from 'react-router-dom';
 import { AuthContext } from '../contexts/auth';
 
 const NavBar = () => {
-    const { logout } = useContext(AuthContext);
+    const { isUserAuthenticated, logout } = useContext(AuthContext);
 
-    const pages = [
-        {
+    const pages = [];
+    const settings = [];
+
+    if (isUserAuthenticated) {
+        pages.push({
             name: 'Home',
             action: '/'
-        },
-        {
-            name: 'Login',
-            action: '/login'
-        },
-        {
-            name: 'Cadastro',
-            action: '/register'
-        }
-    ];
+        });
 
-    const settings = [
-        {
-            name: 'Perfil',
-            action: '/user/profile'
-        },
-        {
-            name: 'Logout',
-            action: logout
-        }
-    ];
+        settings.push(
+            {
+                name: 'Perfil',
+                action: '/user/profile'
+            },
+            {
+                name: 'Logout',
+                action: logout
+            }
+        );
+    } else {
+        pages.push(
+            {
+                name: 'Login',
+                action: '/login'
+            },
+            {
+                name: 'Cadastro',
+                action: '/register'
+            }
+        );
+    }
 
     const [anchorElNav, setAnchorElNav] = useState(null);
     const [anchorElUser, setAnchorElUser] = useState(null);
@@ -130,41 +136,44 @@ const NavBar = () => {
                         ))}
                     </Box>
 
-                    <Box sx={{ flexGrow: 0 }}>
-                        <Tooltip title="Open settings">
-                            <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
-                                <Avatar alt="User" src="/static/images/avatar/2.jpg" />
-                            </IconButton>
-                        </Tooltip>
-                        <Menu
-                            sx={{ mt: '45px' }}
-                            id="menu-appbar"
-                            anchorEl={anchorElUser}
-                            anchorOrigin={{
-                                vertical: 'top',
-                                horizontal: 'right',
-                            }}
-                            keepMounted
-                            transformOrigin={{
-                                vertical: 'top',
-                                horizontal: 'right',
-                            }}
-                            open={Boolean(anchorElUser)}
-                            onClose={handleCloseUserMenu}
-                        >
-                            {settings.map((setting) => (
-                                setting.action !== logout ? (
-                                    <MenuItem key={setting.name} onClick={handleCloseUserMenu} component={RouterLink} to={setting.action}>
-                                        <Typography textAlign="center">{setting.name}</Typography>
-                                    </MenuItem>
-                                ) : (
-                                    <MenuItem key={setting.name} onClick={handleLogout}>
-                                        <Typography textAlign="center">{setting.name}</Typography>
-                                    </MenuItem>
-                                )
-                            ))}
-                        </Menu>
-                    </Box>
+                    {isUserAuthenticated &&
+                        <Box sx={{ flexGrow: 0 }}>
+                            <Tooltip title="Configurações">
+                                <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
+                                    <Avatar alt="User" src="/static/images/avatar/2.jpg" />
+                                </IconButton>
+                            </Tooltip>
+                            <Menu
+                                sx={{ mt: '45px' }}
+                                id="menu-appbar"
+                                anchorEl={anchorElUser}
+                                anchorOrigin={{
+                                    vertical: 'top',
+                                    horizontal: 'right',
+                                }}
+                                keepMounted
+                                transformOrigin={{
+                                    vertical: 'top',
+                                    horizontal: 'right',
+                                }}
+                                open={Boolean(anchorElUser)}
+                                onClose={handleCloseUserMenu}
+                            >
+                                {settings.map((setting) => (
+                                    setting.action !== logout ? (
+                                        <MenuItem key={setting.name} onClick={handleCloseUserMenu} component={RouterLink} to={setting.action}>
+                                            <Typography textAlign="center">{setting.name}</Typography>
+                                        </MenuItem>
+                                    ) : (
+                                        <MenuItem key={setting.name} onClick={handleLogout}>
+                                            <Typography textAlign="center">{setting.name}</Typography>
+                                        </MenuItem>
+                                    )
+                                ))}
+                            </Menu>
+                        </Box>
+                    }
+
                 </Toolbar>
             </Container>
         </AppBar>
